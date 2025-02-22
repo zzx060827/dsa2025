5475b8a847deabb088190766f8628c96997c5597
Marcus-nilE <marcussli2023@163.com>
2025-02-22 16:02:54 +0800

    2400090404.py

--- openjudge/8464/2400090404.py
+++ openjudge/8464/2400090404.py
@@ -1,49 +1,23 @@
-import math
-#solution1
-def max_income_maxk(prices):#时间复杂度O（n）
-    n = len(prices)
-    f = [[[None for i in range(n)]for p in range(3)]for j in range(2)]#记忆化搜索
-    def dfs(i,j,hold):#i表示还剩i天，j表示仍可进行的交易次数，hold表示是否持有股票，1表示持有 0表示不持有
-        if j < 0:
-            return -math.inf
-        elif i < 0 :
-            return -math.inf if hold else 0
-        if f[hold][j][i] != None:
-            return f[hold][j][i]
-        if hold:
-            f[hold][j][i] = max(dfs(i-1,j,1),dfs(i-1,j-1,0)-prices[i])#注意是买入今天的股票
-            return f[hold][j][i]
-        else:
-            f[hold][j][i] = max(dfs(i-1,j,0),dfs(i-1,j,1)+prices[i])#卖出股票#增加一次交易次数
-            return f[hold][j][i]
-    return dfs(n-1,2,0)
-ans = []
-n = int(input())
-for i in range(n):
-    m = int(input())
-    prices = list(map(int,input().split()))
-    ans.append(max_income_maxk(prices))
-for i in ans:
-    print(i)
-
-#solution2
 def maxProfit(prices):
     n = len(prices)
     if n == 0:
         return 0
 
+    # 从左到右计算最大利润
     left_profit = [0] * n
     min_price = prices[0]
     for i in range(1, n):
         min_price = min(min_price, prices[i])
         left_profit[i] = max(left_profit[i - 1], prices[i] - min_price)
 
+    # 从右到左计算最大利润
     right_profit = [0] * n
     max_price = prices[-1]
     for i in range(n - 2, -1, -1):
         max_price = max(max_price, prices[i])
         right_profit[i] = max(right_profit[i + 1], max_price - prices[i])
 
+    # 找到最大的总利润
     max_total_profit = 0
     for i in range(n):
         max_total_profit = max(max_total_profit, left_profit[i] + right_profit[i])
@@ -51,6 +25,7 @@ def maxProfit(prices):
     return max_total_profit
 
 ans=[]
+# 读取输入
 T = int(input())
 for _ in range(T):
     N = int(input())
