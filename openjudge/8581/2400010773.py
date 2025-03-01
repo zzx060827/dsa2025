19a584b396709cfbf6cdabd9eb5a80bc8dc6a4c8
zhangggg217 <z13389271787@qq.com>
2025-03-01 09:37:29 +0800

    a new problem solved

---
+++ openjudge/8581/2400010773.py
@@ -0,0 +1,30 @@
+def build_tree(preorder):
+    if not preorder or preorder[0] == '.':
+        return None, preorder[1:]
+    root = preorder[0]
+    left, preorder = build_tree(preorder[1:])
+    right, preorder = build_tree(preorder)
+    return (root, left, right), preorder
+
+def inorder(tree):
+    if tree is None:
+        return ''
+    root, left, right = tree
+    return inorder(left) + root + inorder(right)
+
+def postorder(tree):
+    if tree is None:
+        return ''
+    root, left, right = tree
+    return postorder(left) + postorder(right) + root
+
+
+preorder = input().strip()
+
+
+tree, x= build_tree(preorder)
+
+
+print(inorder(tree))
+print(postorder(tree))
+
