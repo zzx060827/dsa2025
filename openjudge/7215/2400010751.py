ee23aff0e963334d44fc69cec2498ccc7a966f7f
Mathildaachen <jiahongchen956@gmail.com>
2025-03-19 23:29:58 +0800

    Update 2400010751.py

--- openjudge/7215/2400010751.py
+++ openjudge/7215/2400010751.py
@@ -1,17 +1,3 @@
-def integer_partitions(n):
-    dp = [[0] * (n + 1) for _ in range(n + 1)]
-
-    for j in range(n + 1):
-        dp[0][j] = 1
-    for i in range(1, n + 1):
-        for j in range(1, n + 1):
-            if i < j:
-                dp[i][j] = dp[i][i]
-            else:
-                dp[i][j] = dp[i][j - 1] + dp[i - j][j]
-
-    return dp[n][n]
-
 #感觉和分盘子有点像
 def num_of_plates(n,m):
     if n==0:
