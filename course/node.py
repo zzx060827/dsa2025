aa0999d5ce9329a2caff669fdf397fcce898e36a
m1book <zhengmaoxie@gmail.com>
2025-04-09 17:37:52 +0800

    让代码可以运行

--- course/node.py
+++ course/node.py
@@ -1,25 +1,17 @@
-
-
 class Node:
+    """
+    最简单的单链表结点
+    属性data和next直接访问
+    """
     def __init__(self, dt=None, nxt=None):
-
-
         self.data = dt
         self.next = nxt
 
-
-
-
-
-
 if __name__ == "__main__":
-
     header = None
     for i in range(5):
         header = Node(i, header)
     while header is not None:
         print(header.data)
-        
         header = header.next
-
 
