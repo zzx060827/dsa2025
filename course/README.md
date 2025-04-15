89cc0832d86cfaedab3af01a49a5c2bf065d5b6e
m1book <zhengmaoxie@gmail.com>
2025-04-15 14:07:52 +0800

    使用二叉堆实现的可运行的dijkstra算法

--- course/README.md
+++ course/README.md
@@ -1,6 +1,6 @@
 ## 这个目录下面放的是课程中出现的所有代码
 
-- node.py                    单链表结点类
+- node.py                    单链表结点类，里面实现了一个对链表的插入排序
 - unOrderedList.py           无序表
 - linkedList.py              链表操作作业模版
 - queue.py                   队列实现
@@ -8,4 +8,4 @@
 - graph.py                   图的实现
 - bfs.py                     广度优先搜索
 - wordLadder.py              词梯问题，使用了广度优先搜索
-- dijkstra.py                dijkstra最短路径算法
+- dijkstra.py                dijkstra最短路径算法，使用二叉堆优先队列
--- course/dijkstra.py
+++ course/dijkstra.py
@@ -1,10 +1,43 @@
-from pythonds.graphs import PriorityQueue, Graph, Vertex
+from graph import Graph, Vertex
+from binHeap import BinaryHeap
+import itertools
+
 def dijkstra(aGraph, start):
-    pq = PriorityQueue()
+    entry_finder = {}               # mapping of tasks to entries
+    REMOVED = '<removed-task>'      # placeholder for a removed task
+    counter = itertools.count()     # unique sequence count
+
+    def add_task(task, priority=0):
+        'Add a new task or update the priority of an existing task'
+        if task in entry_finder:
+            remove_task(task)
+        entry = [priority, next(counter), task]
+        entry_finder[task] = entry
+        pq.insert(entry)
+
+    def remove_task(task):
+        'Mark an existing task as REMOVED.  Raise KeyError if not found.'
+        entry = entry_finder.pop(task)
+        entry[-1] = REMOVED
+
+    def pop_task():
+        'Remove and return the lowest priority task. Raise KeyError if empty.'
+        while not pq.isEmpty():
+            priority, count, task = pq.delMin()
+            if task is not REMOVED:
+                del entry_finder[task]
+                return task
+        raise KeyError('pop from an empty priority queue')
+
+    pq = BinaryHeap()
+
     start.setDistance(0)   #对所有顶点建堆，形成优先队列
-    pq.buildHeap([(v.getDistance(), v) for v in aGraph])
+    add_task(start, start.getDistance())
     while not pq.isEmpty():
-        currentVert = pq.delMin()          #优先队列出队
+        try :
+            currentVert = pop_task()          #优先队列出队
+        except KeyError:
+            break
         for nextVert in currentVert.getConnections():
             newDist = currentVert.getDistance() \
                     + currentVert.getWeight(nextVert)
@@ -13,4 +46,25 @@ def dijkstra(aGraph, start):
             if newDist < nextVert.getDistance():
                 nextVert.setDistance(newDist)
                 nextVert.setPred(currentVert)
-                pq.decreaseKey(nextVert, newDist)
+                add_task(nextVert, newDist)
+
+if __name__ == "__main__":
+    def addEdge(g, u, v, weight):
+        g.addEdge(u, v, weight)
+        g.addEdge(v, u, weight)
+
+    g = Graph(Vertex)
+    addEdge(g, 'u', 'v', 2)
+    addEdge(g, 'w', 'v', 3)
+    addEdge(g, 'u', 'w', 5)
+    addEdge(g, 'u', 'x', 1)
+    addEdge(g, 'x', 'v', 2)
+    addEdge(g, 'x', 'w', 3)
+    addEdge(g, 'y', 'w', 1)
+    addEdge(g, 'y', 'x', 1)
+    addEdge(g, 'y', 'z', 1)
+    addEdge(g, 'w', 'z', 5)
+
+    dijkstra(g, g.getVertex('u'))
+    for v in g:
+        print(f'{v.id} : {v.distance}')
