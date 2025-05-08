be21c3e5e8e0ab2b3cf01b4a6254f7b2ffdd926f
m1book <zhengmaoxie@gmail.com>
2025-05-08 14:18:13 +0800

    250505:合并果子采用插入排序的办法，报超时

--- openjudge/14683/2400010956.py
+++ openjudge/14683/2400010956.py
@@ -3,10 +3,15 @@ ap = list(map(int, input().split()))
 ap.sort(reverse = True)
 ans = 0
 while len(ap) > 1:
-    i = -2
-    while i >= -len(ap) and ap[-1] > ap[i]:
-        i -= 1
-    ap[-1], ap[i + 1] = ap[i + 1], ap[-1]
+    last = ap[-1]
+    for i in range(-2, -len(ap)-1, -1):
+        if last > ap[i]:
+            ap[i+1] = ap[i]
+        else:
+            ap[i+1] = last
+            break;
+    else:
+        ap[0] = last
     ap[-2] += ap[-1]
     ap.pop()
     ans += ap[-1]
